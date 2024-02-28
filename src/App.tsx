f4b65f47e40d4edf4a4d714c65d57d5ae1b0a2c3
Mrugesh Vaghela <mrugeshmca1801@gmail.com>
2024-02-28 13:28:36 +0530

    improved logic for rows

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,150 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 interface BubbleProps {
-  number: number,
-  color: string,
-  showBubble: boolean,
+  number: number;
+  color: string;
+  showBubble: boolean;
 }
 /**
  * Component for Bubbles
- * @param param0 
- * @returns 
+ * @param param0
+ * @returns
  */
-const BubbleComponent: React.FC<BubbleProps> = ({ number, color, showBubble }) => {
-  const testId= `test-${number}-${showBubble}`
-  return (
-    showBubble ? <div className={color} data-testid={testId}>{number}</div>: <></>
+const BubbleComponent: React.FC<BubbleProps> = ({
+  number,
+  color,
+  showBubble,
+}) => {
+  const testId = `test-${number}-${showBubble}`;
+  return showBubble ? (
+    <div className={color} data-testid={testId}>
+      {number}
+    </div>
+  ) : (
+    <></>
   );
 };
 
 /**
  * Main app component
- * @returns 
+ * @returns
  */
 function App() {
-  const [ bubbleNumber, setBubbleNumber] = useState('');
+  const [bubbleNumber, setBubbleNumber] = useState("");
   const [buttonDisabled, setButtonDisabled] = useState(true);
-  
-  const initialBubbleList : any  = {
-    '1': false,
-    '2': false,
-    '3': false,
-    '4': false,
-    '5': false,
+  const bubbleStack = [
+    {
+      id: 1,
+      color: "green-bubble",
+      show: false,
+    },
+    {
+      id: 2,
+      color: "pink-bubble",
+      show: false,
+    },
+    {
+      id: 3,
+      color: "red-bubble",
+      show: false,
+    },
+    {
+      id: 4,
+      color: "purple-bubble",
+      show: false,
+    },
+    {
+      id: 5,
+      color: "yellow-bubble",
+      show: false,
+    },
+  ];
+  const initialBubbleList: any = {
+    "1": false,
+    "2": false,
+    "3": false,
+    "4": false,
+    "5": false,
   };
   const [showBubble, setShowBubble] = useState(initialBubbleList);
   const handleChange = (event: any) => {
-    setBubbleNumber(event.target.value);    
-    if(Object.keys(initialBubbleList).includes(event.target.value)) {
-      setButtonDisabled(false);      
+    setBubbleNumber(event.target.value);
+    if (Object.keys(initialBubbleList).includes(event.target.value)) {
+      setButtonDisabled(false);
     } else {
       setButtonDisabled(true);
     }
   };
-  const transferBubble = () => {    
-    console.log('transfer called')
-    setBubbleNumber(bubbleNumber);      
-    if(bubbleNumber) {
+  const transferBubble = () => {
+    setBubbleNumber(bubbleNumber);
+    if (bubbleNumber) {
       showBubble[bubbleNumber] = true;
       setShowBubble(showBubble);
-      setBubbleNumber('');
-    }    
-  }
-/**
- * This method resets bubble positions
- */
-  const resetForm = () =>{
-    setShowBubble(initialBubbleList); 
-  }
-  
+      setBubbleNumber("");
+    }
+  };
+  /**
+   * This method resets bubble positions
+   */
+  const resetForm = () => {
+    setShowBubble(initialBubbleList);
+  };
+
   return (
-    <div className="App">       
-    <div className='main-container'>
-    <div className='bubble-container'>
-        <h1>Container</h1>
-        <div className='bubble-table' data-testid='bubble-table'>
-          <BubbleComponent number = {1} color={'green-bubble'} showBubble={showBubble['1']} data-testid='green-in-table'></BubbleComponent>
-          <BubbleComponent number = {2} color={'pink-bubble'} showBubble={showBubble['2']}></BubbleComponent>
-          <BubbleComponent number = {3} color={'red-bubble'} showBubble={showBubble['3']}></BubbleComponent>
-          <BubbleComponent number = {4} color={'purple-bubble'} showBubble={showBubble['4']}></BubbleComponent>
-          <BubbleComponent number = {5} color={'yellow-bubble'} showBubble={showBubble['5']}></BubbleComponent>
+    <div className="App">
+      {" "}
+       
+      <div className="main-container">
+        <div className="bubble-container">
+          <h1>Container</h1>
+          {bubbleStack.map((bubble) => {
+            return (
+              <BubbleComponent
+                number={bubble.id}
+                color={bubble.color}
+                showBubble={showBubble[bubble.id]}
+              ></BubbleComponent>
+            );
+          })}
         </div>
-        
+        <div className="bubble-stack">
+          {bubbleStack.map((bubble) => {
+            return (
+              <BubbleComponent
+                number={bubble.id}
+                color={bubble.color}
+                showBubble={!showBubble[bubble.id]}
+              ></BubbleComponent>
+            );
+          })}
+        </div>
+        <input
+          data-testid="bubble-number"
+          type="text"
+          name="bubbleNumber"
+          placeholder={"Enter 1 to 5"}
+          className="input"
+          value={bubbleNumber}
+          onChange={handleChange}
+        ></input>
+        <button
+          data-testid="swap-button"
+          type="button"
+          className="button"
+          onClick={transferBubble}
+          disabled={buttonDisabled}
+        >
+          Transfer
+        </button>
+        <button
+          data-testid="reset-button"
+          type="button"
+          className="button"
+          onClick={resetForm}
+        >
+          Reset
+        </button>
       </div>
-        <div className='bubble-stack'>          
-          <BubbleComponent  number = {1} color={'green-bubble'} showBubble={!showBubble['1']} ></BubbleComponent>
-          <BubbleComponent number = {2} color={'pink-bubble'} showBubble={!showBubble['2']}></BubbleComponent>
-          <BubbleComponent number = {3} color={'red-bubble'} showBubble={!showBubble['3']}></BubbleComponent>
-          <BubbleComponent number = {4} color={'purple-bubble'} showBubble={!showBubble['4']}></BubbleComponent>
-          <BubbleComponent number = {5} color={'yellow-bubble'}showBubble={!showBubble['5']}></BubbleComponent>
-        </div>
-        <input data-testid='bubble-number' type='text' name='bubbleNumber' placeholder={'Enter 1 to 5'} className='input' value={bubbleNumber} onChange={handleChange} ></input>
-        <button data-testid='swap-button' type='button' className='button' onClick={transferBubble} disabled={buttonDisabled}>Transfer</button>
-        <button data-testid='reset-button' type='button' className='button' onClick={resetForm} >Reset</button>
-      </div>      
-      
     </div>
   );
 }
